6c0c4c95b00495e113ce52479accb42647ddd0b3
Tomas Hernandez <pedrothdc@gmail.com>
2020-04-26 12:39:50 +0200

    Package fixes Link target and noopener

--- src/components/article-card/article-card-component.jsx
+++ src/components/article-card/article-card-component.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
-import noimg from'../../images/picture.svg';
+import noimg from '../../images/picture.svg';
 const getPrettyDate = (date) => {
-    let d = new Date(date);
-    return `${d.getFullYear()}/${d.getMonth()}/${d.getDate()}`
-}
+  let d = new Date(date);
+  return `${d.getFullYear()}/${d.getMonth()}/${d.getDate()}`;
+};
 export const ArticleCard = (props) => {
-    return (
-        <article key={props.publishedAt} className="card">
-            <a href={props.url} className="reorder" target="_blank"><h2>{props.title}</h2></a>
-            <div className="img-container">
-                <img src={(props.urlToImage)?props.urlToImage: noimg} className={(props.urlToImage)?'': 'noimage'}  alt="article main" />               
-                </div>
-            <div className="content">
-                <span className="date">{getPrettyDate(props.publishedAt)}</span>
-                <p>{props.description}</p>
-                <span className="readmore-link"><a href={props.url} target="_blank">Read More</a></span>
-                </div>
-        </article>
-    )
-}
+  return (
+    <article key={props.publishedAt} className="card">
+      <a
+        href={props.url}
+        className="reorder"
+        target="_blank"
+        without
+        rel="noopener noreferrer"
+      >
+        <h2>{props.title}</h2>
+      </a>
+      <div className="img-container">
+        <img
+          src={props.urlToImage ? props.urlToImage : noimg}
+          className={props.urlToImage ? '' : 'noimage'}
+          alt="article main"
+        />
+      </div>
+      <div className="content">
+        <span className="date">{getPrettyDate(props.publishedAt)}</span>
+        <p>{props.description}</p>
+        <span className="readmore-link">
+          <a href={props.url} target="_blank" without rel="noopener noreferrer">
+            Read More
+          </a>
+        </span>
+      </div>
+    </article>
+  );
+};
